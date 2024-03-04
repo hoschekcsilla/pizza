62c6997a1317ae9461c35118e4ea61ae8b40520a
hoschekcsilla <160332467+hoschekcsilla@users.noreply.github.com>
2024-03-04 16:02:10 +0100

    Update javascript.js
    kosar zárás-nyitás

--- javascript.js
+++ javascript.js
@@ -19,3 +19,15 @@ setInterval(changePlateImage, 4000);
 // Kezdéskor beállítjuk az első képet
 changePlateImage();
 
+let kosarka=document.querySelector('#kosar')
+let kosarkam=document.querySelector('.kosarr')
+let zaroKosarka=document.querySelector('#zaro')
+
+kosarka.onclick= () =>{
+  kosarkam.classList.add("active");
+};
+
+zaroKosarka.onclick= () =>{
+  kosarkam.classList.remove("active");
+};
+
